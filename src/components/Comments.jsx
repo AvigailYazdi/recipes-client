7517fdea79a68a034982112f9c71807791c4d389
Avigail Yazdi <ayazdi@g.jct.ac.il>
2026-04-15 11:11:39 +0300

    Showing system response in recipe comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -5,9 +5,12 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useAddComment } from "../hooks/useAddComment";
 import { formatRelativeTime } from "../utils/dateUtils";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 export const Comments = (props) => {
-  const {openAuthDialog} = useContext(AuthContext);
+  const [openComments, setOpenComments] = useState([]);
+  const { openAuthDialog } = useContext(AuthContext);
   const { recipeId } = props;
   const [newComment, setNewComment] = useState("");
   const { token } = useContext(AuthContext);
@@ -27,6 +30,16 @@ export const Comments = (props) => {
     );
   };
 
+  const toggleReply = (commentId) => {
+    if (openComments.includes(commentId)) {
+      setOpenComments((prev) =>
+        prev.filter((comment) => comment !== commentId),
+      );
+    } else {
+      setOpenComments((prev) => [...prev, commentId]);
+    }
+  };
+
   return (
     <div>
       {isLoading && <CircularProgress />}
@@ -36,15 +49,43 @@ export const Comments = (props) => {
         <div className="all-comment-div">
           <h2 className="title-with-lines">{comments.length} תגובות</h2>
           {comments.map((comment) => (
-            <div className="comment-div" key={comment._id}>
-              <span className="comment-name">{comment.userId?.name || "משתמש"}</span>
-              <div className="comment-date-div">
-                <AccessTimeIcon sx={{ fontSize: 13 }} />
-                <span className="date">
-                  {formatRelativeTime(comment.createdAt)}
+            <div key={comment._id}>
+              <div className="comment-div">
+                <span className="comment-name">
+                  {comment.userId?.name || "משתמש"}
                 </span>
+                <div className="comment-date-div date">
+                  <AccessTimeIcon sx={{ fontSize: 13 }} />
+                  <span>{formatRelativeTime(comment.createdAt)}</span>
+                </div>
+                <span className="comment-text">{comment.text}</span>
+                {comment.adminReply?.text && (
+                  <div className="admin-reply-div">
+                    <div
+                      className="system-reply"
+                      onClick={() => toggleReply(comment._id)}
+                    >
+                      {openComments.includes(comment._id) ? (
+                        <KeyboardArrowUpIcon sx={{ fontSize: 16 }} />
+                      ) : (
+                        <KeyboardArrowDownIcon sx={{ fontSize: 16 }} />
+                      )}
+                      <span>תגובת מערכת</span>
+                    </div>
+                    {openComments.includes(comment._id) && (
+                      <div>
+                        <div className="comment-date-div date">
+                          <AccessTimeIcon sx={{ fontSize: 13 }} />
+                          <span>
+                            {formatRelativeTime(comment.adminReply.createdAt)}
+                          </span>
+                        </div>
+                        <span>{comment.adminReply.text}</span>
+                      </div>
+                    )}
+                  </div>
+                )}
               </div>
-              <span className="comment-text">{comment.text}</span>
               <Divider />
             </div>
           ))}
@@ -54,13 +95,24 @@ export const Comments = (props) => {
             maxLength={800}
             placeholder="התגובה שלך"
             value={newComment}
-            disabled={token===null}
+            disabled={!token}
             onChange={(e) => setNewComment(e.target.value)}
           />
           {token === null ? (
-            <span className="not-yet-connected" onClick={()=>openAuthDialog("login")}>עוד לא מחובר? התחבר על מנת להוסיף תגובה</span>
+            <span
+              className="not-yet-connected"
+              onClick={() => openAuthDialog("login")}
+            >
+              עוד לא מחובר? התחבר על מנת להוסיף תגובה
+            </span>
           ) : (
-            <button className="btn send-comment-btn" onClick={handleSubmit} disabled={newComment.trim() === ""}>שלח תגובה</button>
+            <button
+              className="btn send-comment-btn"
+              onClick={handleSubmit}
+              disabled={newComment.trim() === ""}
+            >
+              שלח תגובה
+            </button>
           )}
         </div>
       )}
